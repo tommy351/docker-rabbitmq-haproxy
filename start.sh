7d04374eceff79e48e9e0a6fa6e2c0c73c8a056a
Tommy Chen <tommy351@gmail.com>
2016-11-24 13:40:08 +0800

    First commit

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+# Add cookie
+cookie_path=/var/lib/rabbitmq/.erlang.cookie
+
+echo "ERLANGCOOKIE" > $cookie_path
+chmod 400 $cookie_path
+chown rabbitmq:rabbitmq $cookie_path
+
+# Initialize configs
+for p in 5673 5674 5675
+do
+  echo "
+[program:rabbitmq-$p]
+command=rabbitmq-server
+environment=RABBITMQ_NODENAME=\"rabbit-$p@localhost\",RABBITMQ_NODE_PORT=\"$p\"
+autorestart=unexpected
+startsecs=5
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
+" >> /supervisord.conf
+done
+
+# Start supervisor
+supervisord
